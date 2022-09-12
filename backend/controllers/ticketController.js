d6191812d76ed345bc8d4afceeb0dbe93793882d
Quang Hung Pham <hungpham307@gmail.com>
2022-09-12 17:10:25 +0700

    perform ticket crud in the backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -17,6 +17,34 @@ const getTickets = asyncHandler(async (req, res) => {
     res.status(200).json(tickets)
 })
 
+//* Perform CRUD for single ticket with id in params
+
+/* @description: Get user ticket
+@route: GET /api/tickets/:id
+@access: private */
+const getUserTicket = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    /* get ticket by a specific user */
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    /* not allow a user to see others' ticket */
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error("Cannot see others' ticket")
+    }
+
+    res.status(200).json(ticket)
+})
+
 /* @description: create new ticket
 @route: POST /api/tickets
 @access: private */
@@ -44,4 +72,61 @@ const createTicket = asyncHandler(async (req, res) => {
     res.status(201).json(ticket)
 })
 
-module.exports = { getTickets, createTicket }
+/* @description: Get user ticket
+@route: DELETE /api/tickets/:id
+@access: private */
+const deleteTicket = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    /* get ticket by a specific user */
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    /* not allow a user to see others' ticket */
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error("Cannot see others' ticket")
+    }
+    await ticket.remove()
+
+    res.status(200).json({ success: true })
+})
+
+
+/* @description: Get user ticket
+@route: PUT /api/tickets/:id
+@access: private */
+const updateTicket = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    /* get ticket by a specific user */
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    /* not allow a user to see others' ticket */
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error("Cannot see others' ticket")
+    }
+
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedTicket)
+})
+
+
+module.exports = { getTickets, getUserTicket, createTicket, deleteTicket, updateTicket }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getTickets, createTicket } = require('../controllers/ticketController')
+const { getTickets, getUserTicket, createTicket, deleteTicket, updateTicket } = require('../controllers/ticketController')
 const { protect } = require('../middleware/authMiddleware')
 
 /* route('/') can chain on multiple methods */
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+router.route('/:id').get(protect, getUserTicket).delete(protect, deleteTicket).put(protect, updateTicket)
 
 module.exports = router
